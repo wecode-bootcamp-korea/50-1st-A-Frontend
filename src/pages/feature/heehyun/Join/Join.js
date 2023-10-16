338e3d515ac79af2b262735a50235e08aec47d4b
shin.heehyun <lupinworld22@gmail.com>
2023-10-16 15:39:36 +0900

    기존 작업 페이지 컴포넌트 복사

---
+++ src/pages/feature/heehyun/Join/Join.js
@@ -0,0 +1,210 @@
+import React from "react";
+import "./Join.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Join = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordVerify, setPasswordVerify] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+
+  const setChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const setChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const setChangePasswordVerify = (event) => {
+    setPasswordVerify(event.target.value);
+  };
+
+  const setChangeNickname = (event) => {
+    setNickname(event.target.value);
+  };
+
+  const setChangeMobileNumber = (event) => {
+    setMobileNumber(event.target.value);
+  };
+
+  const [user, setUser] = useState([]);
+
+  const goToLogin = () => {
+    navigate("/");
+  };
+
+  const signUp = () => {
+    if (email.indexOf('@') === -1 || email.indexOf('.') === -1) {
+      alert('이메일 형식이 아닙니다.');
+    } else if (password !== passwordVerify) {
+      alert('비밀번호가 다릅니다.');
+    } else {
+      const newUser = {
+        id: user.length + 1,
+        email: email,
+        password: password,
+        nickname: nickname,
+        mobileNumber: mobileNumber,
+      };
+
+      setUser([...user, newUser]);
+      alert('회원가입이 완료되었습니다!');
+      navigate("/");
+    }
+  };
+
+  const [form, setForm] = useState({
+    year: "연도",
+    month: "월",
+    day: "일",
+  });
+
+  const now = new Date();
+
+  let years = [];
+  for (let y = now.getFullYear(); y >= 1930; y -= 1) {
+    years.push(y.toString() + "년");
+  }
+
+  let months = [];
+  for (let m = 1; m <= 12; m += 1) {
+    months.push(m.toString() + "월");
+  }
+
+  let days = [];
+  for (let d = 1; d <= 31; d += 1) {
+    days.push(d.toString() + "일");
+  }
+
+  return (
+    <div className="signup">
+      <div className="header">
+        <button onClick={goToLogin} className="back-btn">
+          ﹤ 뒤로
+        </button>
+        <div></div>
+      </div>
+
+      <div className="header-title">
+        <div className="title">회원가입</div>
+        <div></div>
+      </div>
+
+      <div className="personalInfo">
+        <div className="detail">
+          <div>기본 정보</div>
+          <div className="remark">필수 사항</div>
+        </div>
+        <input
+          onChange={setChangeEmail}
+          value={email}
+          className="input email"
+          type="text"
+          placeholder="이메일"
+        ></input>
+
+        <input
+          className="input password"
+          type="password"
+          placeholder="비밀번호"
+          onChange={setChangePassword}
+          value={password}
+        ></input>
+
+        <input
+          className="input password-verify"
+          type="password"
+          placeholder="비밀번호 확인"
+          onChange={setChangePasswordVerify}
+          value={passwordVerify}
+        ></input>
+
+        <div className="detail">
+          <div>닉네임과 프로필 이미지</div>
+          <div className="non-remark">선택 사항</div>
+        </div>
+        <input
+          className="input nickname"
+          type="text"
+          placeholder="닉네임"
+          onChange={setChangeNickname}
+          value={nickname}
+        ></input>
+
+        <div className="detail">
+          <div>전화번호</div>
+          <div className="non-remark">선택 사항</div>
+        </div>
+        <div className="mobile-number">
+          <select className="pre-number">
+            <option>010</option>
+            <option>011</option>
+            <option>016</option>
+            <option>017</option>
+            <option>019</option>
+          </select>
+          <input
+            className="input mobile"
+            type="text"
+            placeholder="휴대폰 번호를 입력해주세요."
+            onChange={setChangeMobileNumber}
+            value={mobileNumber}
+          ></input>
+        </div>
+
+        <div className="detail">
+          <div>생일</div>
+          <div className="non-remark">선택 사항</div>
+        </div>
+        <div className="birth">
+          <select
+            className="birthyear"
+            value={form.year}
+            onChange={(e) => setForm({ ...form, year: e.target.value })}
+          >
+            {years.map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+          <select
+            className="birthmonth"
+            value={form.month}
+            onChange={(e) => setForm({ ...form, month: e.target.value })}
+          >
+            {months.map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+          <select
+            className="birthday"
+            value={form.day}
+            onChange={(e) => setForm({ ...form, day: e.target.value })}
+          >
+            {days.map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button
+          onClick={signUp}
+          className="joinbtn">
+          회원 가입
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
---
+++ src/pages/feature/heehyun/Login/Login.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "./Login.scss";
+import logo1 from "./Logo.png";
+import logo2 from "./logo_wecode.png";
+import { useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+const Login = () => {
+  // 라우터를 통해 네비게이트 함수 활성화
+  const navigate = useNavigate();
+
+  // 이메일, 비밀번호 저장할 State 사용 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // 이메일, 비밀번호를 객체로 저장할 State 사용
+  const [user, setUser] = useState([]);
+
+  // 이메일, 비밀번호를 저장할 함수 사용
+  const setChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const setChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  // 버튼을 활성화시킬 수 있는 active 변수 사용
+  const [active, setActive] = useState(false);
+
+  // 로그인 버튼을 눌렀을때 이메일과 비밀번호를 저장하면서 동시에 메인 페이지 이동
+  const goToMain = () => {
+    const newUser = {
+      id: user.length + 1,
+      email: email,
+      password: password,
+    };
+
+    setUser([...user, newUser]);
+    navigate('/main');
+  };
+
+  // Enter키를 누르면 로그인화면 전환
+  const handleOnClick = (event) => {
+    if (event.key === 'Enter' && active === true) {
+      goToMain();
+    }
+  };
+
+  // 이메일에 '@'와 ','이 포함되어 있으며 비밀번호의 길이가 5글자 이상일 때 로그인 버튼 활성화
+  const passedLogin = () => {
+    return email.includes('@') && password.length >= 5 && email.includes('.')
+      ? setActive(true)
+      : setActive(false);
+  };
+
+  // 회원가입 페이지 이동 함수
+  const goToJoin = () => {
+    navigate('/join');
+  };
+
+  return (
+    <div className="login">
+      <div className="logo-board">
+        <img src={logo1} alt="로고1" />
+        <img src={logo2} alt="로고2" />
+      </div>
+
+      <div className="email-password">
+        <input onKeyUp={passedLogin} className="input" type="text" placeholder="이메일" value={email} onChange={setChangeEmail} onKeyPress={handleOnClick}></input>
+
+        <input onKeyUp={passedLogin} className="input" type="password" placeholder="비밀번호" value={password} onChange={setChangePassword} onKeyPress={handleOnClick}></input>
+
+        {/* 로그인이 가능하면 초록색으로 전환 */}
+        <button
+          onClick={goToMain} className={!active ? "loginbtn" : "loginbtn-access"} disabled={!active}>로그인</button>
+      </div>
+
+      <div className="buttons">
+        <button onClick={goToJoin} className="btn join">회원 가입</button>
+
+        <div>|</div>
+
+        <button className="btn find">비밀번호 찾기</button>
+      </div>
+    </div >
+  );
+};
+
+export default Login;
