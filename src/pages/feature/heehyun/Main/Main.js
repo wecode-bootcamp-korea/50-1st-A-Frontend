41f2753070342cda46a7d067b9155c418bc95270
shin.heehyun <lupinworld22@gmail.com>
2023-10-16 15:42:23 +0900

    기존 작업물 복사 붙여넣기

--- src/pages/feature/heehyun/Main/Main.js
+++ src/pages/feature/heehyun/Main/Main.js
@@ -1,27 +1,32 @@
-import React from "react";
-import "./Main.scss";
+import React from 'react';
+import './Main.scss';
 
 const Main = () => {
-  return <div className="main">
-    <div className="post">
-      <div className="posting-box">
-        <div className="header">
-          <div className="profile">
-            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="프로필 사진" className="profile-image"></img>
-            <div className="nickname">Name</div>
+  return (
+    <div className="main">
+      <div className="post">
+        <div className="posting-box">
+          <div className="header">
+            <div className="profile">
+              <img
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                alt="프로필 사진"
+                className="profile-image"
+              />
+              <div className="nickname">Name</div>
+            </div>
+            <div className="posting-date">00.00.00</div>
           </div>
-          <div className="posting-date">00.00.00</div>
+          <div className="detail">내용</div>
+          <button className="comment-link">댓글 00</button>
         </div>
-        <div className="detail">
-          내용
-        </div>
-        <button className="comment-link">댓글 00</button>
+      </div>
+
+      <div className="footer">
+        <button className="btn write">글 쓰기</button>
       </div>
     </div>
-
-    <div className="footer">
-      <button className="btn write">글 쓰기</button></div>
-  </div>;
+  );
 };
 
 export default Main;
